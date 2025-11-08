691370eab2d32e741310e078e7f0feacfb320c5b
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-08 18:24:53 +0000

    boletin 1 - variables y condicionales - ejercicio 15 - switch / case

--- src/variables_y_condicionales15/Main.java
+++ src/variables_y_condicionales15/Main.java
@@ -2,7 +2,7 @@ package variables_y_condicionales15;
 
 import util.Entrada;
 
-// import util.ConsoleColors;
+import util.ConsoleColors;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,32 +10,69 @@ public class Main {
 
         System.out.println("Ejercício 15 - Pedir el dia, mes y año de una fecha e indicar si la fecha es correcta. Suponiendo todos los meses de 30 dias. \n");
 
+        System.out.println("1. (dia - condition: 1 to 30) \n");
+        System.out.println("2. (mes - condition: 1 to 12) \n");
+        System.out.println("3. (ano - condition: ano > 0) \n");
+
         int dia, mes, ano;
+        String error = "e"; // y = year (año); m = month (mes); d = day (dia)
 
         System.out.println("Type a day (1 to 30): \n");
         dia = Entrada.entero();
 
-        while(dia < 1 | dia > 30) {
-            System.out.println("\nNot allowed: Please, type a number between 1 and 30: \n");
-            dia = Entrada.entero();
-        };
-
         System.out.println("\nType a month (1 to 12): \n");
         mes = Entrada.entero();
 
-        while(mes < 1 | mes > 12) {
-            System.out.println("\nNot allowed: Please, type a number between 1 and 12: \n");
-            mes = Entrada.entero();
-        };
-
-        System.out.println("\nType a year (between 1900 & 2100): \n");
+        System.out.println("\nType a year (> 0): \n");
         ano = Entrada.entero();
 
-        while(ano < 1900 | ano > 2100) {
-            System.out.println("\nNot allowed: Please, type a number between 1900 and 2100: \n");
-            ano = Entrada.entero();
+        System.out.println("\nLa fecha es la seguinte: " + dia + "/" + mes + "/" + ano + "\n");
+
+        if (dia < 1 | dia > 30) {
+            error += "d";
+        };
+        
+        if (mes < 1 | mes > 12) {
+            error += "m";
+        };
+        
+        if (ano < 1) {
+            error += "y";
         };
 
-        System.out.println("\nLa fecha es la seguinte: " + dia + "/" + mes + "/" + ano + "\n");
+        if (error == "e") {
+            System.out.println(ConsoleColors.success("La fecha es correcta! \n"));
+        } else {
+            System.out.println(ConsoleColors.error("La fecha es incorrecta! \n"));
+        };
+
+        // end program here, bellow is extra code for error handling display
+
+        switch(error) {
+            case "ed":
+                System.out.println(ConsoleColors.error("INVALID VALUE - DIA: " + dia + "\n"));
+                break;
+            case "em":
+                System.out.println(ConsoleColors.error("INVALID VALUE - MES - (condition: 1 to 12): " + mes + "\n"));
+                break;
+            case "ey":
+                System.out.println(ConsoleColors.error("INVALID VALUE - ANO: " + ano + "\n"));
+                break;
+            case "edm":
+                System.out.println(ConsoleColors.error("INVALID VALUES - DIA: " + dia + " MES: " + mes + "\n"));
+                break;
+            case "edy":
+                System.out.println(ConsoleColors.error("INVALID VALUES - DIA: " + dia + " ANO: " + ano + "\n"));
+                break;
+            case "emy":
+                System.out.println(ConsoleColors.error("INVALID VALUES - MES: " + mes + " ANO: " + ano + "\n"));
+                break;
+            case "edmy":
+                System.out.println(ConsoleColors.error("INVALID VALUES - DIA: " + dia + " MES: " + mes + " ANO: " + ano + "\n"));
+                break;
+            default:
+                System.out.println(ConsoleColors.success(dia + "/" + mes + "/" + ano + "\n"));
+                break;
+        };
     };
 };
---
+++ src/variables_y_condicionales16/Main.java
@@ -0,0 +1,14 @@
+package variables_y_condicionales16;
+
+import util.ConsoleColors;
+
+public class Main {
+    public static void main (String[] args) {
+        System.out.println(ConsoleColors.success("Hello world!"));
+
+        System.out.println("\nBoletin 1 - Variables y condicionales \n");
+
+        System.out.println("Ejercício 16 - Pedir el dia, mes y año de una fecha e indicar si la fecha es correcta. Suponiendo todos los meses de 30 dias. \n");
+
+    };
+};
