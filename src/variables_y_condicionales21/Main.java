bb4921b25fc9ef2ca4ae56616cdf229d05a67d28
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-16 19:26:29 +0000

    boletin 1 - variables y condicionales - ejercicio 22

--- src/variables_y_condicionales21/Main.java
+++ src/variables_y_condicionales21/Main.java
@@ -6,8 +6,6 @@ import util.ConsoleColors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         System.out.println(
                 "\nEjercicio 21 - Pedir una nota numérica entera entre 0 y 10, y mostrar dicha nota de la forma: cero, uno, dos, tres, ...\n");
 
--- src/variables_y_condicionales22/Main.java
+++ src/variables_y_condicionales22/Main.java
@@ -1,14 +1,31 @@
 package variables_y_condicionales22;
 
-// import util.Entrada;
+import util.Entrada;
 
-// import util.ConsoleColors;
+import util.ConsoleColors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         System.out.println(
                 "\nEjercicio 22 - Pedir un número de 0 a 99 y mostrarlo escrito. Por ejemplo, para 56 mostrar: cincuenta y seis.\n");
+
+        int number;
+
+        // String numString;
+
+        System.out.println("Please, type a number between 0 and 99: \n");
+        number = Entrada.entero();
+
+        if (number >= 0 && number < 100) {
+            System.out.println(ConsoleColors.success("The number is in range."));
+
+            System.out.println((number - (number % 10)) / 10);
+
+            System.out.println(number % 10);
+
+        } else {
+            System.out.println(ConsoleColors.error("The number is out of range."));
+        }
+
     };
 }
