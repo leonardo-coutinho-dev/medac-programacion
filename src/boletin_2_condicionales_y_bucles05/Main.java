973c222ccd5fa1a941d63ee44bcd16c8dd6af694
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-25 11:07:50 +0000

    boletin 2 - condicionales y bucles - ejercicio 05

---
+++ src/boletin_2_condicionales_y_bucles05/Main.java
@@ -0,0 +1,33 @@
+package boletin_2_condicionales_y_bucles05;
+
+import util.*;
+
+public class Main {
+    public static void main() {
+        System.out.println(
+                "\n5. Realizar un juego para adivinar un número. Para ello pedir un número N, y luego ir pidiendo números indicando “mayor” o “menor” según sea mayor o menor con respecto a N. El proceso termina cuando el usuario acierta.\n");
+
+        int N, number;
+
+        System.out.println("Please, enter the number to be guessed:\n");
+        N = Entrada.entero();
+
+        System.out.println("\nTry to guess the number:\n");
+        number = Entrada.entero();
+
+        while (number != N) {
+
+            if (number < N) {
+                System.out.println(ConsoleColors.error("\nThe number you typed is lower, try higher! (+) \n"));
+                number = Entrada.entero();
+            } else {
+                System.out.println(ConsoleColors.error("\nThe number you typed is higher, try lower! (-) \n"));
+                number = Entrada.entero();
+            }
+
+        }
+
+        System.out.println(ConsoleColors.success("\nCongratulations, you guessed!\n"));
+
+    }
+}
