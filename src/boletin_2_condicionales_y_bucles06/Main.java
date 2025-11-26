bc86e53d0a8be533abb39ba3ccd01afc1acbd189
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-26 11:46:44 +0000

    boletin 2 - condicionales y bucles - ejercicio 6

---
+++ src/boletin_2_condicionales_y_bucles06/Main.java
@@ -0,0 +1,25 @@
+package boletin_2_condicionales_y_bucles06;
+
+import util.*;
+
+public class Main {
+    public static void main() {
+        System.out.println(
+                "\n6. Pedir números hasta que se teclee un 0, mostrar la suma de todos los números introducidos.\n");
+
+        int number, sum = 0;
+
+        System.out.println("Please, type a number:\n");
+        number = Entrada.entero();
+
+        while (number != 0) {
+            sum += number;
+
+            System.out.println("\nPlease, type another number:\n");
+            number = Entrada.entero();
+        }
+
+        System.out.println("\n" + sum);
+
+    }
+}
