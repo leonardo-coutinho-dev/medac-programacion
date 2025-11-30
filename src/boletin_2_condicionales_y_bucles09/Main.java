2e4ceb25ebcf9b6b8bed746a52feeacee0ea2661
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-11-30 23:45:10 +0000

    boletin 2 - condicionales y bucles - ejercicio 09

---
+++ src/boletin_2_condicionales_y_bucles09/Main.java
@@ -0,0 +1,24 @@
+package boletin_2_condicionales_y_bucles09;
+
+public class Main {
+    public static void main() {
+        System.out.println("\n9. Escribir todos los números del 100 al 0 de 7 en 7.\n");
+
+        int i = 100;
+
+        System.out.println("Con while: \n");
+
+        while (i >= 0) {
+            System.out.println(i + "\n");
+            i = i - 7;
+        }
+
+        System.out.println("Con for: \n");
+
+        for (int c = 100; c >= 0; c -= 7) {
+            System.out.println(c + "\n");
+        }
+
+        // cleaner code with for
+    }
+}
