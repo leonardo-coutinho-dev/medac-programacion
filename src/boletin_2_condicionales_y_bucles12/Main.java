66c5a39f5b9a8cb0c3ebc29a2112e6d228497a8d
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-12-05 23:36:18 +0000

    boletin 2 - condicionales y bucles - ejercicio 12 - file created

---
+++ src/boletin_2_condicionales_y_bucles12/Main.java
@@ -0,0 +1,11 @@
+package boletin_2_condicionales_y_bucles12;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nHello, world!\n");
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
