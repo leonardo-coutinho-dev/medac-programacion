e9da5bb9723d93b01c2a090bc0cc62843d6b5b50
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-12-10 23:57:48 +0000

    boletin 2 - condicionales y bucles - ejercicio 12

--- src/boletin_2_condicionales_y_bucles12/Main.java
+++ src/boletin_2_condicionales_y_bucles12/Main.java
@@ -1,11 +1,23 @@
 package boletin_2_condicionales_y_bucles12;
 
+import util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nHello, world!\n");
+        System.out.println("\n12. Pedir un número y calcular su factorial.\n");
 
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
+        int number, factorial = 1;
+
+        System.out.println("Type a number: ");
+        number = Entrada.entero();
+
+        System.out.println("This is the number you entered: " + number);
+
+        for (int i = number; i > 0; i--) {
+            factorial *= i;
+        }
+
+        System.out.println(factorial);
+
     }
 }
