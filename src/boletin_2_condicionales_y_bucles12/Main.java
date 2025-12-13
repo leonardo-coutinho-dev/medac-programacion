eb7ff72227e5cbd5ddf8088a6f10efb074dc1319
Leonardo Coutinho dos Santos <web.dev.trabalho@gmail.com>
2025-12-13 23:31:49 +0000

    boletin 2 - condicionales y bucles - ejercicio 12 - using BigInteger

--- src/boletin_2_condicionales_y_bucles12/Main.java
+++ src/boletin_2_condicionales_y_bucles12/Main.java
@@ -2,13 +2,17 @@ package boletin_2_condicionales_y_bucles12;
 
 import java.util.ArrayList;
 
+import java.math.BigInteger;
+
 import util.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("\n12. Pedir un número y calcular su factorial.\n");
 
-        int number, factorial = 1;
+        int number = 1;
+
+        BigInteger factorial_bigInteger = BigInteger.ONE;
 
         String operation = "";
 
@@ -19,25 +23,23 @@ public class Main {
 
         System.out.println("\nThis is the number you entered: " + number + "\n");
 
-        for (int i = number; i > 0; i--) {
-            factorial *= i;
+        operation += number + "! = ";
 
-            if (i == 1) {
-                operation += i;
-            } else {
-                operation += i + "x";
-            }
+        for (int i = number; i > 0; i--) {
+            factorial_bigInteger = factorial_bigInteger.multiply(BigInteger.valueOf(i));
+
+            // if (i == 1) {
+            // operation += i;
+            // } else {
+            // operation += i + " x ";
+            // }
 
             numbers.add(i);
         }
 
-        // System.out.println(factorial);
+        // System.out.println(operation + " = " + factorial_bigInteger + "\n");
 
-        // System.out.println("\n" + operation + "\n");
-
-        // System.out.println(numbers + "\n");
-
-        System.out.println(operation + " = " + factorial + "\n");
+        System.out.println(operation + factorial_bigInteger);
 
     }
 }
